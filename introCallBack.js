868e06f271e7569e755ea50dde7d2061520a1b49
Brian Wu <wubrian1234@hotmail.com>
2018-11-29 19:27:28 +0000

    solve for each

--- introCallBack.js
+++ introCallBack.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(element, index){
+    if (element === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
@@ -12,3 +12,6 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
